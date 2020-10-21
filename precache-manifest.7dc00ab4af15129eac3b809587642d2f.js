361e86a6e1027e0f0d40dd4a85264f01a3a182a0
YosaRama <yosamelody07@gmail.com>
2020-10-22 02:34:06 +0800

    Updates

---
+++ precache-manifest.7dc00ab4af15129eac3b809587642d2f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a1c6d04b3e38591128793412d7dd0f6",
+    "url": "/Contact-App/index.html"
+  },
+  {
+    "revision": "2b3ad47408bae628b172",
+    "url": "/Contact-App/static/css/0.6fab625c.chunk.css"
+  },
+  {
+    "revision": "e5e145570349cf47503d",
+    "url": "/Contact-App/static/css/main.343c5528.chunk.css"
+  },
+  {
+    "revision": "2b3ad47408bae628b172",
+    "url": "/Contact-App/static/js/0.6e3dcc48.chunk.js"
+  },
+  {
+    "revision": "ecef787518253a5c6da06dfb3c82bca1",
+    "url": "/Contact-App/static/js/0.6e3dcc48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1bb154729ff8859417d",
+    "url": "/Contact-App/static/js/3.12a0604e.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Contact-App/static/js/3.12a0604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54484432d736e7c6b71f",
+    "url": "/Contact-App/static/js/4.ca214e05.chunk.js"
+  },
+  {
+    "revision": "e49cc0f567b9300c4a8f",
+    "url": "/Contact-App/static/js/5.93dbf25d.chunk.js"
+  },
+  {
+    "revision": "dff1f786fe47e1bdb2bf",
+    "url": "/Contact-App/static/js/6.dd479220.chunk.js"
+  },
+  {
+    "revision": "ee3e3c9516dfff4ec59b",
+    "url": "/Contact-App/static/js/7.aad864da.chunk.js"
+  },
+  {
+    "revision": "e5e145570349cf47503d",
+    "url": "/Contact-App/static/js/main.671a0ff5.chunk.js"
+  },
+  {
+    "revision": "68d88dfe52614ca62467",
+    "url": "/Contact-App/static/js/runtime-main.52a67501.js"
+  },
+  {
+    "revision": "d4b454201d8e5d578fc43e61426e1fab",
+    "url": "/Contact-App/static/media/profile.d4b45420.jpg"
+  }
+]);
